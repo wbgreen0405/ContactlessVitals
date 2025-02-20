8918eca2ba6b196caed864cbb72b25824ca27f4f
William Green <williamrgre@gmail.com>
2025-02-20 12:35:44 -0600

    Update ui_app.py

--- ContactlessVitals/frontend/streamlit_app/ui_app.py
+++ ContactlessVitals/frontend/streamlit_app/ui_app.py
@@ -6,9 +6,9 @@ st.write("Choose a page below:")
 
 # If your pages are named "1_Splash.py", "2_Onboarding.py", etc., do:
 
-st.page_link(page="1_Splash", label="Splash", icon="🌅")
-st.page_link(page="2_Onboarding", label="Onboarding", icon="📝")
-st.page_link(page="3_Home", label="Home", icon="🏠")
-st.page_link(page="4_Measurement", label="Measurement", icon="🎥")
-st.page_link(page="5_History", label="History & Trends", icon="📈")
+st.page_link(page="1_Splash.py", label="Splash", icon="🌅")
+st.page_link(page="2_Onboarding.py", label="Onboarding", icon="📝")
+st.page_link(page="3_Home.py", label="Home", icon="🏠")
+st.page_link(page="4_Measurement.py", label="Measurement", icon="🎥")
+
 
